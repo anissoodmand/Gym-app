0010a72b4720af1cbe1446e93c1a8fa1236d82c9
Anis-Soodmand <soodmandanis@gmail.com>
2025-07-08 12:22:05 +0330

    helpppppp

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,17 @@ import express from 'express';
 import { connectDB } from './config/database';
 import dotenv from 'dotenv';
 import authRoutes from './api/auth/auth.routes'
-
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+
+app.use(cors({
+  origin: 'https://p-a-gym.netlify.app/', // or specify your frontend like 'http://localhost:5174'
+}));
 
 app.use('/api/auth' , authRoutes)
 connectDB();
