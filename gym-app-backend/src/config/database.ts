a7d9172d7159007af3ce0485cd3e02e348798ede
Anis-Soodmand <soodmandanis@gmail.com>
2025-07-08 08:35:27 +0330

    setting

--- gym-app-backend/src/config/database.ts
+++ gym-app-backend/src/config/database.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const mongo_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/gym-app'
+const mongo_URI = process.env.MONGO_URI as string;
 
 export const connectDB = async () =>{
     try {
