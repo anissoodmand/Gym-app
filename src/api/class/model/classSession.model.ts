ed5bfd3ff55decc9c163fb894b0cb86a4922acac
Anis-Soodmand <soodmandanis@gmail.com>
2025-08-09 11:07:37 +0330

    Generate Session***

--- src/api/class/model/classSession.model.ts
+++ src/api/class/model/classSession.model.ts
@@ -4,15 +4,19 @@ export interface IClassSession extends Document {
  scheduleId: Types.ObjectId;
  date: Date;
  registeredUsers: Types.ObjectId[];
- isCanceled: boolean;   
+ isCanceled: boolean;  
+ startTime : string;
+ endTime: string; 
 }
 
 const ClassSessionSchema = new Schema<IClassSession>(
     {
         scheduleId: {type: Schema.Types.ObjectId , ref: "ClassSchedule" ,required: true},
-         date: { type: Date, required: true },
-         registeredUsers: [{type: Schema.Types.ObjectId , ref: "User" }],
-          isCanceled: { type: Boolean, default: false }
+        date: { type: Date, required: true },
+        registeredUsers: [{type: Schema.Types.ObjectId , ref: "User" }],
+        isCanceled: { type: Boolean, default: false },
+        startTime: { type: String, required: true },
+        endTime: { type: String, required: true }
   },
   { timestamps: true }
 );
