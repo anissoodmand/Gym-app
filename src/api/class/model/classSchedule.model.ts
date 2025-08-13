9b7c8d4433c5379c063a012f73efe48a24a2e9b3
Anis-Soodmand <soodmandanis@gmail.com>
2025-08-13 11:12:34 +0330

    coachId***

--- src/api/class/model/classSchedule.model.ts
+++ src/api/class/model/classSchedule.model.ts
@@ -1,4 +1,4 @@
-import mongoose,{ Schema , Document} from "mongoose";
+import mongoose,{ Schema , Document, Types} from "mongoose";
 export interface ColumnRowByDay {
   day: "شنبه"|"یک شنبه"|"دو شنبه"|"سه شنبه"|"چهار شنبه"|"پنج شنبه"|"جمعه";
   column: number;
@@ -8,10 +8,12 @@ export interface IClassSchedule extends Document{
     title : string;
     category: 'کراس فیت' | "پیلاتس"| "فیتنس"| "آمادگی جسمانی"| "ایروبیک";
     coach: string;
+    coachId: Types.ObjectId;
     days: ColumnRowByDay[];
     startTime: string;
     endTime: string;
     capacity: number;
+     price: number;
     isActive: boolean;
 }
 const columnRowByDaySchema = new Schema(
@@ -33,10 +35,12 @@ const ClassScheduleSchema = new Schema<IClassSchedule>(
     title: { type: String, required: true },
     category: { type: String, required: true },
     coach: { type: String, required: true },
+    coachId: {type: Schema.Types.ObjectId, ref: 'Coach', required: true},
     days:  [columnRowByDaySchema],
     startTime: { type: String, required: true },
     endTime: { type: String, required: true },
     capacity: { type: Number, required: true },
+     price: { type: Number, required: true },
     isActive: { type: Boolean, default: true },
   },
   { timestamps: true }
