d41a0fa09929b1630e07ab08c8b1a05221021a28
Anis-Soodmand <soodmandanis@gmail.com>
2025-08-25 20:21:49 +0330

    session history

--- src/api/bodybuilding/model/sessionHistory.model.ts
+++ src/api/bodybuilding/model/sessionHistory.model.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface ISessionHistory extends Document {
   enrollmentId: mongoose.Types.ObjectId; // به کدوم اشتراک وصله
   userId: mongoose.Types.ObjectId;
-  coachId: mongoose.Types.ObjectId;
+  coachId?: mongoose.Types.ObjectId;
   packageId: mongoose.Types.ObjectId;
   usedAt: Date; // تاریخ ثبت حضور
 }
@@ -12,7 +12,7 @@ const sessionHistorySchema = new Schema<ISessionHistory>(
   {
     enrollmentId: { type: Schema.Types.ObjectId, ref: "PackageEnrollment", required: true },
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    coachId: { type: Schema.Types.ObjectId, ref: "Coach", required: true },
+    coachId: { type: Schema.Types.ObjectId, ref: "Coach", },
     packageId: { type: Schema.Types.ObjectId, ref: "Package", required: true },
     usedAt: { type: Date, default: Date.now },
   },
